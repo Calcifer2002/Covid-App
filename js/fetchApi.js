bf2d7a2a345d392785bd44acbc217b4f3633c8db
Calcifer2002 <83289730+Calcifer2002@users.noreply.github.com>
2021-10-21 01:38:54 +0530

    Update fetchApi.js

--- js/fetchApi.js
+++ js/fetchApi.js
@@ -10,7 +10,7 @@ document.getElementById("searchBtn").addEventListener('click', function (e) {
 })
 function getInputValue(){
     var inputVal = document.getElementById("myInput").value;
-    fetch("https://corona.lmao.ninja/v2/countries/"+inputVal)
+    fetch("https://corona.lmao.ninja/v2/countries/"+{inputVal})
   .then((response) => {
     return response.json();
   })
